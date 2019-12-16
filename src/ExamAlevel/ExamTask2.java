0baf3bba001d62aa64d807e79b5f94a9e16f5b02
vlad <vladyslav.gurin@gmail.com>
2019-12-16 21:22:54 +0200

    "Task 2"

--- src/ExamAlevel/ExamTask2.java
+++ src/ExamAlevel/ExamTask2.java
@@ -1,4 +1,25 @@
 package ExamAlevel;
 
-public class ExamTask2 {
+import java.util.Scanner;
+
+public class ExamTask2 { //Даны точки A,B,C на плоскости. Вычислить площадь треугольника ABC.
+    public static void main(String[] args) {
+
+       // int x1, x2, x3, y1, y2, y3;
+        double square;
+        Scanner scanner = new Scanner(System.in);
+        int x1 = scanner.nextInt();
+        int x2 = scanner.nextInt();
+        int x3 = scanner.nextInt();
+        int y1 = scanner.nextInt();
+        int y2 = scanner.nextInt();
+        int y3 = scanner.nextInt();
+
+        square = (1/2) * (((x1-x3)*(y2-y3))- ((x2-x3)*(y2-y3)));
+        if (square < 0)
+            square = - square;
+        System.out.println("Square of trangle equels: " + square);
+
+
+    }
 }
